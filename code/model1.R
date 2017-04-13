5f57373dd91fc99da891d8758bba2dba8ab0248e
Philip Bulsink <bulsinkp@gmail.com>
2017-04-13 15:51:16 -0400

    Some models.

--- code/model1.R
+++ code/model1.R
@@ -2,33 +2,22 @@
 library(syuzhet)
 library(pbapply)
 
-q1<-train$question1
-q2<-train$question2
+train$sq1<-get_sentiment(train$question1)
+train$sq2<-get_sentiment(train$question2)
 
-s1<-get_nrc_sentiment(q1)
-s2<-get_nrc_sentiment(q2)
-
-rm(q1, q2)
-
-colnames(s1)<-paste("q1_", colnames(s1), sep="")
-colnames(s2)<-paste("q2_", colnames(s2), sep="")
-
-train<-cbind(train, s1, s2)
-
-rm(s1, s2)
-
+#try vectorize
 strDups<-function(x){
-  a<-unlist(tokenize(x['question1']))
-  b<-unlist(tokenize(x['question2']))
+  a<-unlist(tokenize(x['question1b']))
+  b<-unlist(tokenize(x['question2b']))
   return(sum(a%in%b)/sum(length(a), length(b)))
 }
 
 strCapDiff<-function(x){
   #returns capital words per sentence in Q1 minus capital words per sentence in Q2
-  a<-unlist(tokenize(x['question1']))
-  a_sent<-length(unlist(tokenize(x['question1'], what='sentence')))
-  b<-unlist(tokenize(x['question2']))
-  b_sent<-length(unlist(tokenize(x['question2'], what='sentence')))
+  a<-unlist(tokenize(x['question1b']))
+  a_sent<-length(unlist(tokenize(x['question1b'], what='sentence')))
+  b<-unlist(tokenize(x['question2b']))
+  b_sent<-length(unlist(tokenize(x['question2b'], what='sentence')))
   a<-a[!(a %in% b)]
   b<-b[!(b %in% a)]
   a_cap<-sum(grepl("[A-Z][a-zA-Z]+", a)) 
@@ -42,8 +31,17 @@ strCapDiff<-function(x){
 #pbapply not parallelized? Issue#24 https://github.com/psolymos/pbapply/issues/24
 train$dup_prop<-pbapply(train, 1, function(x) strDups(x))#, cl=cl)
 train$cap_diff<-pbapply(train, 1, function(x) strCapDiff(x))#, cl=cl)
+test$dup_prop<-pbapply(test, 1, function(x) strDups(x))#, cl=cl)
+test$cap_diff<-pbapply(test, 1, function(x) strCapDiff(x))#, cl=cl)
 
 #stopCluster(cl)
+
+#train$question1b<-NULL
+#train$question2b<-NULL
+train$qid1<-NULL
+train$qid2<-NULL
+
+train$is_duplicate<-as.factor(train$is_duplicate)
 
 library(caret)
 inTraining<-createDataPartition(train$is_duplicate, p=0.75, list=FALSE)
@@ -53,6 +51,9 @@ val<-train[-inTraining,]
 set.seed(1)
 
 fitControl <- trainControl(method = 'repeatedcv', number=10)
-
-model1<-train(is_duplicate ~ ., data=tr, method="rf")
-model2<-train(is_duplicate ~ ., data=tr, method='gbm')
+# 
+#model1<-train(is_duplicate ~ ., data=val, method="rf", na.action = na.omit)
+model_gbm<-train(is_duplicate ~ sq1+sq2+dup_prop+cap_diff, data=tr, method='gbm', trControl = fitControl, na.action = na.omit, verbose=FALSE)
+model_svm<-train(is_duplicate ~ sq1+sq2+dup_prop+cap_diff, data=tr, method='svmRadial', trControl = fitControl, na.action = na.omit, verbose=FALSE)
+model_rda<-train(is_duplicate ~ sq1+sq2+dup_prop+cap_diff, data=tr, method='rda', trControl = fitControl, na.action = na.omit, verbose=FALSE)
+gbm_pre<-predict(object = )
